a243486abca76d7e3f8bc0b9201dd354e38864b7
Nhu Thuy <tranthinhuthuyd1tp@gmail.com>
2024-04-10 11:54:50 +0700

    [Frontend Mentor] nft preview card

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    extend: {
+      colors: {
+        "soft-blue": "hsl(215, 51%, 70%)",
+        "cyan": "hsl(178, 100%, 50%)",
+        "main-bg": "hsl(217, 54%, 11%)",
+        "card-bg": "hsl(216, 50%, 16%)",
+        "line": "hsl(215, 32%, 27%)",
+      },
+    },
+  },
+  plugins: [],
+}
+
