4eee0bd2646e94ae697d43ac3f95b849787c18dd
Nhu Thuy <tranthinhuthuyd1tp@gmail.com>
2024-04-10 13:00:39 +0700

    update active states

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,9 +6,13 @@ module.exports = {
       colors: {
         "soft-blue": "hsl(215, 51%, 70%)",
         "cyan": "hsl(178, 100%, 50%)",
+        "cyan-layer": "hsla(178, 100%, 50%, 50%)",
         "main-bg": "hsl(217, 54%, 11%)",
         "card-bg": "hsl(216, 50%, 16%)",
         "line": "hsl(215, 32%, 27%)",
+      },
+      backgroundImage: {
+        'view': "url('../images/icon-view.svg')",
       },
     },
   },
